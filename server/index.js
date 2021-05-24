72fc6086f8266e5344e8b0ba6af711f821c02073
jyotijoey <jburnwal5@gmail.com>
2021-05-24 13:38:06 +0530

    set up message handler on the backend

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const http = require("http");
 const router = require("./router.js");
 const cors = require("cors");
 const app = express();
+const {addUser, removeUser, getUser, getUsersInRoom} = require("./users");
 
 app.use(router);
 app.use(cors());
@@ -18,12 +19,27 @@ const io = socketio(server, {cors: {
 
 
 io.on("connection", (socket) => {
-    console.log("connected");
 
     socket.on("join", ({name, room}, callback) => {
-        
-        console.log(callback);
+        const {error, user} = addUser({ id: socket.id, name, room});
+
+        if(error) return callback(error);
+
+        socket.emit("message", {user:"admin", text:`${user.name}, welcome to the room ${user.room}`});
+
+        socket.broadcast.to(room).emit("message", {user:"admin", text:`${user.name}, has joined the room!`});
+
+        socket.join(user.room);
+
+        callback();
+    });
+
+    socket.on("sendMessage", (message, callback) => {
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit("message");
     })
+
     socket.on("disconnect", () => {
         console.log("User has left");
     })
